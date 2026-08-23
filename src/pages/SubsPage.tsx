cf90d77d9ae505b770b2ef3e0d1ed6a96beb846f
cardellishawn-cloud <Cardellishawn1@gmail.com>
2026-08-22 22:00:14 -0400

    Add SubsPage

---
+++ src/pages/SubsPage.tsx
@@ -0,0 +1,8 @@
+export default function SubsPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Subs</h1>
+      <p className="text-gray-600">Subs page coming soon.</p>
+    </div>
+  );
+}
